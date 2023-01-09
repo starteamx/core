ae7c34ebd3119ec3739bd48a7300e6b6c7b3398d
guanrunbai <470626708@qq.com>
2023-01-09 16:54:00 +0800

    增加lib-network,判断网络状态相关方法

---
+++ lib-network/src/main/java/com/starteam/core/lib/network/NetworkType.java
@@ -0,0 +1,34 @@
+package com.starteam.core.lib.network;
+
+/**
+ * author : guanrunbai
+ * time   : 2023/01/04
+ * desc   : 网络相关枚举类
+ * version: 1.0
+ */
+public enum NetworkType {
+    /**
+     * wifi
+     */
+    NETWORK_WIFI,
+    /**
+     * 4g
+     */
+    NETWORK_4G,
+    /**
+     * 3g
+     */
+    NETWORK_3G,
+    /**
+     * 2g
+     */
+    NETWORK_2G,
+    /**
+     * 未知网络
+     */
+    NETWORK_UNKNOWN,
+    /**
+     * 其他
+     */
+    NETWORK_NO
+}
